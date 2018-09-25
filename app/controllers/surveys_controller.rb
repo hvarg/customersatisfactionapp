dc0c45cdda0c87b08a318b1351c687fa2dbb68bb
cjsalame <cjsalame@uc.cl>
2018-09-25 17:49:21 -0300

    working for all browsers

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -3,36 +3,36 @@ class SurveysController < ApplicationController
 
   # GET /surveys
   # GET /surveys.json
-  def index
-    @surveys = Survey.all
-  end
-
-  # GET /surveys/1
-  # GET /surveys/1.json
-  def show
-  end
+  # def index
+  #   @surveys = Survey.all
+  # end
+  #
+  # # GET /surveys/1
+  # # GET /surveys/1.json
+  # def show
+  # end
 
   # GET /surveys/new
   def new
     @survey = Survey.new
   end
 
-  # GET /surveys/1/edit
-  def edit
-  end
+  # # GET /surveys/1/edit
+  # def edit
+  # end
 
   # POST /surveys
   # POST /surveys.json
   def create
-    if params[:commit] == '1'
+    if params[:very_bad] != nil
         @survey = Survey.new(score: 2)
-    elsif params[:commit] == '2'
+    elsif params[:bad] != nil
         @survey = Survey.new(score: 4)
-    elsif params[:commit] == '3'
+    elsif params[:neutral] != nil
         @survey = Survey.new(score: 6)
-    elsif params[:commit] == '4'
+    elsif params[:good] != nil
         @survey = Survey.new(score: 8)
-    elsif params[:commit] == '5'
+    elsif params[:very_good] != nil
         @survey = Survey.new(score: 10)
     end
     # @survey.attention_desk_id = 1
@@ -47,38 +47,38 @@ class SurveysController < ApplicationController
     end
   end
 
-  # PATCH/PUT /surveys/1
-  # PATCH/PUT /surveys/1.json
-  def update
-    respond_to do |format|
-      if @survey.update(survey_params)
-        format.html { redirect_to @survey, notice: 'Survey was successfully updated.' }
-        format.json { render :show, status: :ok, location: @survey }
-      else
-        format.html { render :edit }
-        format.json { render json: @survey.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /surveys/1
-  # DELETE /surveys/1.json
-  def destroy
-    @survey.destroy
-    respond_to do |format|
-      format.html { redirect_to surveys_url, notice: 'Survey was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_survey
-      @survey = Survey.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    # def survey_params
-    #   params.require(:survey).permit()
-    # end
+  # # PATCH/PUT /surveys/1
+  # # PATCH/PUT /surveys/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @survey.update(survey_params)
+  #       format.html { redirect_to @survey, notice: 'Survey was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @survey }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @survey.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+  #
+  # # DELETE /surveys/1
+  # # DELETE /surveys/1.json
+  # def destroy
+  #   @survey.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to surveys_url, notice: 'Survey was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
+  # 
+  # private
+  #   # Use callbacks to share common setup or constraints between actions.
+  #   def set_survey
+  #     @survey = Survey.find(params[:id])
+  #   end
+  #
+  #   # Never trust parameters from the scary internet, only allow the white list through.
+  #   # def survey_params
+  #   #   params.require(:survey).permit()
+  #   # end
 end

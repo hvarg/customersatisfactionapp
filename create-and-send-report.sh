9ab8ed58c22fd3d220a4581346b3e906b4a255ca
Hernán Vargas <hvargasl@interior.gob.cl>
2019-03-04 17:54:20 -0300

    fixed email content

--- create-and-send-report.sh
+++ create-and-send-report.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 filename=reports/$(date +%m-%y.csv -d 'last month')
 echo "COPY (SELECT created_at, score FROM surveys WHERE created_at > now() - interval '1' month) TO STDOUT WITH CSV DELIMITER ',';" | psql -o $filename customersatisfactionapp_development
-mailx -a $filename -s "Reporte mensual satisfacción de usuarios" "some@mail.com" < reports/mail_cont.txt
+cat reports/mail_cont.txt | mailx -A gmail -s "Reporte mensual satisfaccion de usuarios" -a $filename mail@gmail.com

4a31e7456f7608bb8e17468ada6f6e7f5474e434
Cloud User <ec2-user@ip-172-31-43-43.us-east-2.compute.internal>
2019-03-05 10:43:11 -0300

    fixed attchments and email body

--- create-and-send-report.sh
+++ create-and-send-report.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-filename=reports/$(date +%m-%y.csv -d 'last month')
-echo "COPY (SELECT created_at, score FROM surveys WHERE created_at > now() - interval '1' month) TO STDOUT WITH CSV DELIMITER ',';" | psql -o $filename customersatisfactionapp_development
-cat reports/mail_cont.txt | mailx -A gmail -s "Reporte mensual satisfaccion de usuarios" -a $filename mail@gmail.com
+address=hvargasl@interior.gob.cl
+subject="Reporte mensual satisfaccion de usuarios"
+body="Se adjuntan los datos de satisfaccion de usuario correspondientes al mes anterior."
+dbname=customersatisfactionapp_development
+query="COPY (SELECT created_at, score FROM surveys WHERE created_at > now() - interval '1' month) TO STDOUT WITH CSV DELIMITER ',';"
+filename=/home/ec2-user/customersatisfactionapp/reports/$(date +%m-%y.csv -d 'last month')
+
+echo $query | psql -o $filename $dbname
+echo $body | mailx -A gmail -s "$subject" -a $filename $address
+#The 'gmail' account must be configured in ~/.mailrc
